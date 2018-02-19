f567d6b0d0fefff452bd09300a18d5983bed920a
arc82 <arc82@cam.ac.uk>
2018-02-19 22:39:00 +0000

    Added yaw control, untested

--- decision_making/src/main.py
+++ decision_making/src/main.py
@@ -8,22 +8,49 @@ class DecisionMaking():
     def __init__(self):
 
         self.river_side_data = []
+        self.current_yaw = 0
+        self.desired_yaw = 0
+
+        # These are hysteresis boundaries (radians) for keeping the river sides vertical (on the sonar image) 
+        self.vertical_river_angle_lower_thresh = 0.1
+        self.vertical_river_angle_higher_thresh = 0.2
+        self.use_upper_bound = False #False if using lower bound, true if using upper bound
         
         rospy.init_node('decision_making')
         self.yaw_publisher = rospy.Publisher('yaw_demand', std_msgs.msg.String, queue_size = 10)
-        rospy.Subscriber('river_sides', std_msgs.msg.Float32MultiArray, self.set_river_side_data)
+        rospy.Subscriber('river_sides', std_msgs.msg.Float32MultiArray, self.river_sides_callback)
+        rospy.Subscriber('current_yaw', std_msgs.msg.Float32, self.current_yaw_callback)
 
         self.rate = rospy.Rate(10)
 
-    def set_river_side_data(self, data):
+    def river_sides_callback(self, data):
         if not(len(data.data) == 0):
             self.river_side_data = data.data
+
+    def current_yaw_callback(self, data):
+        print "current_yaw_callback setting self.current_yaw to: ", data.data
+        # Is it necessary to add a check that the data is not 0?
+        self.current_yaw = data.data
+
+    def update_desired_yaw(self):
+        if not(len(self.river_side_data) == 0):
+            avg_river_side_angle = (self.river_side_data[2] + self.river_side_data[3]) / 2            
+
+            if self.use_upper_bound:
+                if abs(avg_river_side_angle) > self.vertical_river_angle_higher_thresh:
+                    self.use_upper_bound = False
+                    self.desired_yaw = self.current_yaw  - avg_river_side_angle
+            else:
+                if abs(avg_river_side_angle) > self.vertical_river_angle_lower_thresh:
+                    self.use_upper_bound = True
+                    self.desired_yaw = self.current_yaw - avg_river_side_angle
+
             
 
     def run(self):
         while not rospy.is_shutdown():
-            print "Decision making river side data: "
-            print self.river_side_data
+            self.update_desired_yaw() 
+            self.yaw_publisher.publish(str(self.desired_yaw)) #Why can't we just use a Float32 as opposed to a string? 
 
             self.rate.sleep()
 
